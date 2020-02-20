9ebad513647852979e79d59cd2ce856ee7b070c2
loqiu_eclipse <loqiu_software_eclipse@163.com>
2020-02-20 16:21:20 +0800

    适配器方法在指针和enum接口中的转换

---
+++ src/AdapterPattern/SimpleAdapter/EnumerationIterator.java
@@ -0,0 +1,31 @@
+package AdapterPattern.SimpleAdapter;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class EnumerationIterator implements Iterator<Object> {
+
+	Enumeration<Object> enumer;
+
+	public EnumerationIterator(Enumeration<Object> enumer) {
+		// TODO Auto-generated constructor stub
+		this.enumer = enumer;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return enumer.hasMoreElements();
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return enumer.nextElement();
+	}
+
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
